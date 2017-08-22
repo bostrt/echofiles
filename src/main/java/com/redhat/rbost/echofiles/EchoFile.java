07a36abe5b0fad39c5de39972a0aeb2d39fb48b1
Robert Bost <bostrt@gmail.com>
2017-08-22 17:44:20 -0400

    indent fix

--- src/main/java/com/redhat/rbost/echofiles/EchoFile.java
+++ src/main/java/com/redhat/rbost/echofiles/EchoFile.java
@@ -21,61 +21,64 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 @WebServlet("/echo")
 public class EchoFile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private static final Logger log = Logger.getLogger(EchoFile.class.getSimpleName());
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EchoFile() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public EchoFile() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		      
-	      try {
-		      ServletFileUpload upload = new ServletFileUpload();
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+			ServletFileUpload upload = new ServletFileUpload();
 
 			FileItemIterator iter = upload.getItemIterator(request);
-			
-		      while (iter.hasNext())
-		      {
-		        FileItemStream item = iter.next();
-		        String name = item.getFieldName();
-		        InputStream stream = item.openStream();
 
-		          String filename = item.getName();
-		          
-		          response.setContentType("application/octet-stream");
-		          response.setHeader("Content-Disposition", "attachment;filename=" + filename + ".html");
+			while (iter.hasNext()) {
+				FileItemStream item = iter.next();
+				String name = item.getFieldName();
+				InputStream stream = item.openStream();
 
-		          byte[] buffer = new byte['x'];
-		          int count = 0;
-		          int n = 0;
-		          while (-1 != (n = stream.read(buffer))) {
-		        	  log.warning("======");
-		        	  response.getOutputStream().write(buffer, 0, n);
-		        	  log.warning("buffer.length: " + buffer.length);
-		        	  log.warning("bytes read: " + n);
-		        	  log.warning("======");
-		        	  count += n;
-		          }
-		          log.warning("Finished reading, flushing buffer. Total " + count + " bytes.");
-		          stream.close();
-		          response.flushBuffer();
-		      }
+				String filename = item.getName();
+
+				response.setContentType("application/octet-stream");
+				response.setHeader("Content-Disposition", "attachment;filename=" + filename + ".html");
+
+				byte[] buffer = new byte['x'];
+				int count = 0;
+				int n = 0;
+				while (-1 != (n = stream.read(buffer))) {
+					log.warning("======");
+					response.getOutputStream().write(buffer, 0, n);
+					log.warning("buffer.length: " + buffer.length);
+					log.warning("bytes read: " + n);
+					log.warning("======");
+					count += n;
+				}
+				log.warning("Finished reading, flushing buffer. Total " + count + " bytes.");
+				stream.close();
+				response.flushBuffer();
+			}
 		} catch (FileUploadException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
